cb5a3b783d8c9e9b5867c6fa47a9c3cdab9768f0
CFdefense <CFdefence@gmail.com>
2024-10-04 11:26:37 -0400

    added anime effect for fall finish

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -96,8 +96,13 @@ const HomePage = () => {
     });
 
     // Start the hide sign animation
-    delay(2000).then(() => {
+    delay(1800).then(() => {
       setStartAnimation(false); // Hide the sign after some time
+    });
+    
+    // final big shake
+    delay(3900).then(() => {
+      bigShake();
     });
   };
 
@@ -117,6 +122,34 @@ const HomePage = () => {
             { value: -10, duration: shakeInterval / 2 },
             { value: 10, duration: shakeInterval / 2 },
             { value: 0, duration: shakeInterval / 2 }
+          ],
+          easing: 'easeInOutQuad',
+        });
+      } else {
+        clearInterval(interval);
+      }
+    }, shakeInterval);
+  };
+  const bigShake = () => {
+    const shakeInterval = 150; // Interval for shaking
+    const shakeDuration = 300; // Total duration for shake
+    const startTime = Date.now();
+
+    const interval = setInterval(() => {
+      const elapsed = Date.now() - startTime;
+
+      if (elapsed < shakeDuration) {
+        anime({
+          targets: 'body',
+          translateX: [
+            { value: -100, duration: shakeInterval / 3 },
+            { value: 50, duration: shakeInterval / 3 },
+            { value: -100, duration: shakeInterval / 3 },
+            { value: 50, duration: shakeInterval / 3 },
+          ],
+          translateY: [
+            { value: -100, duration: 200, easing: 'easeInOutQuad' }, // Upward movement
+            { value: 0, duration: 200, easing: 'easeOutBounce' } // Bounce back to original position
           ],
           easing: 'easeInOutQuad',
         });
